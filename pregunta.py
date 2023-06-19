396424475fec2b38a44dc1ca30ee19867afde9cd
Jaime A <jaimeangel1097@gmail.com>
2023-06-19 11:26:30 -0500

    intento 01/falta setear las fechas

--- pregunta.py
+++ pregunta.py
@@ -16,5 +16,14 @@ def clean_data():
     #
     # Inserte su código aquí
     #
+    df=df.dropna()
+
+    df['sexo'] = df['sexo'].str.lower();
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower();
+    df['idea_negocio']=df['idea_negocio'].apply(lambda x: x.lower().replace("_", " ").replace("-", " "))
+    df['barrio']=df['barrio'].apply(lambda x: x.lower().replace("_"," ").replace("-"," "))
+    df['línea_credito']=df['línea_credito'].apply(lambda x: x.lower().replace("-"," ").replace("_"," "))
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
 
     return df
+print(clean_data())
